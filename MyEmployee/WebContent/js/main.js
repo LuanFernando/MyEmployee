4bb0fa9ff5e990e7627bc6d8d7acd6619b4627fc
Mlocal01 <Mlocal01@192.168.1.4>
2017-11-21 08:42:27 -0200

    System holerite.

--- MyEmployee/WebContent/js/main.js
+++ MyEmployee/WebContent/js/main.js
@@ -2,7 +2,11 @@
  * Passa os dados do cliente para o Modal, e atualiza o link para exclusão
  */
 $('#delete-modal').on('show.bs.modal', function (event) {
-  var modal = $(this);
   
-  modal.find('.modal-title').text('Confirmar Exclusão');
+  var button = $(event.relatedTarget);
+  var id = button.data('customer');
+  
+  var modal = $(this);
+  modal.find('.modal-title').text('Excluir Cliente #' + id);
+  modal.find('#confirm').attr('href', 'delete.php?id=' + id);
 })
